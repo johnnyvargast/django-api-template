6b270ce8c6bcb800cb9e802bb56ecf6bdcd65a7b
javiervt <javier@elemento43.com>
2021-06-26 13:24:32 -0500

    feat: middleware of library 'model_log'

--- backend/settings.py
+++ backend/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'model_log.middleware.ModelLogMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
